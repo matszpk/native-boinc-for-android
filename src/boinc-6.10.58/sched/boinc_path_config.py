78a34cf3654d6c47dce24bc156653049a0a4bdc1
matszpk <matszpk@interia.pl>
2011-07-08 21:11:41 +0200

    Update configure.ac

--- src/boinc-6.10.58/sched/boinc_path_config.py
+++ src/boinc-6.10.58/sched/boinc_path_config.py
@@ -8,5 +8,5 @@
 
 import sys, os
 
-TOP_BUILD_DIR = "/home/mat/docs/src/android/boinc-6.10.58"
+TOP_BUILD_DIR = "/home/mat/docs/src/android/native-boinc-for-android/src/boinc-6.10.58"
 sys.path.insert(0, os.path.join(TOP_BUILD_DIR, 'py'))

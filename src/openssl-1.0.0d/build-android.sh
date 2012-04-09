55cb3e2c756241da42df3c921c94a429b8f0e903
matszpk <matszpk@interia.pl>
2012-04-09 11:27:18 +0200

    Fixed scripts for curl and openssl. Also removed obsolete files.

--- src/openssl-1.0.0d/build-android.sh
+++ src/openssl-1.0.0d/build-android.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./Configure linux-generic32 no-idea no-bf no-cast no-seed no-md2 no-shared no-dso -DL_ENDIAN --openssldir="$BOINCROOTDIR/ssl"
+./Configure linux-generic32 no-shared no-dso -DL_ENDIAN --openssldir="$BOINCROOTDIR/ssl"
 
 sed -e "s/^CFLAG=.*$/`grep -e \^CFLAG= Makefile` \$(CFLAGS)/g
 s%^INSTALLTOP=.*%INSTALLTOP=$BOINCROOTDIR%g" Makefile > Makefile.out

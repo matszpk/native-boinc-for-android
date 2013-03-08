6bd79e73b508ce980a43696819fba387aee9eaa0
matszpk <matszpk@interia.pl>
2013-03-08 18:24:47 +0100

    Fixed init.sh. Fixed configure.ac in boinc 6.10.58

--- src/init.sh
+++ src/init.sh
@@ -12,22 +12,22 @@ export CC=$NDKTOOLPATH/arm-linux-androideabi-gcc
 export CXX=$NDKTOOLPATH/arm-linux-androideabi-g++
 export LD=$NDKTOOLPATH/arm-linux-androideabi-ld
 
-#export CFLAGS="--sysroot=$SYSROOT -Wall -I$BOINCROOTDIR/include"
-#export CFLAGS="--sysroot=$SYSROOT -Wall -I$BOINCROOTDIR/include -O3 -mhard-float -mfpu=vfp -mfloat-abi=softfp -fomit-frame-pointer"
-#export CFLAGS="--sysroot=$SYSROOT -Wall -I$BOINCROOTDIR/include -O3 -mhard-float -march=armv7-a -mfpu=neon -mtune=cortex-a9 -mfloat-abi=softfp -fomit-frame-pointer"
-export CFLAGS="--sysroot=$SYSROOT -Wall -I$BOINCROOTDIR/include -O3 -fomit-frame-pointer"
-#export CFLAGS="--sysroot=$SYSROOT -Wall -I$BOINCROOTDIR/include -g"
+#export CFLAGS="--sysroot=$SYSROOT -Wall"
+#export CFLAGS="--sysroot=$SYSROOT -Wall -O3 -mhard-float -mfpu=vfp -mfloat-abi=softfp -fomit-frame-pointer"
+#export CFLAGS="--sysroot=$SYSROOT -Wall -O3 -mhard-float -march=armv7-a -mfpu=neon -mtune=cortex-a9 -mfloat-abi=softfp -fomit-frame-pointer"
+export CFLAGS="--sysroot=$SYSROOT -Wall -O3 -fomit-frame-pointer"
+#export CFLAGS="--sysroot=$SYSROOT -Wall -g"
 
-export GDB_CFLAGS="--sysroot=$SYSROOT -Wall -g -I$BOINCROOTDIR/include"
+export GDB_CFLAGS="--sysroot=$SYSROOT -Wall -g"
 #export CXXFLAGS="--sysroot=$SYSROOT -Wall -funroll-loops -fexceptions"
-#export CXXFLAGS="--sysroot=$SYSROOT -Wall -I$BOINCROOTDIR//include -O3 -mhard-float -mfpu=vfp -mfloat-abi=softfp -fexceptions -fomit-frame-pointer"
-#export CXXFLAGS="--sysroot=$SYSROOT -Wall -I$BOINCROOTDIR/include -O3 -mhard-float -march=armv7-a -mfpu=neon -mtune=cortex-a9 -mfloat-abi=softfp -fexceptions -fomit-frame-pointer"
+#export CXXFLAGS="--sysroot=$SYSROOT -Wall -O3 -mhard-float -mfpu=vfp -mfloat-abi=softfp -fexceptions -fomit-frame-pointer"
+#export CXXFLAGS="--sysroot=$SYSROOT -Wall -O3 -mhard-float -march=armv7-a -mfpu=neon -mtune=cortex-a9 -mfloat-abi=softfp -fexceptions -fomit-frame-pointer"
 export CXXFLAGS="--sysroot=$SYSROOT -Wall -funroll-loops -fexceptions -O3 -fomit-frame-pointer"
-#export CXXFLAGS="--sysroot=$SYSROOT -Wall -I$BOINCROOTDIR/include -g -fno-rtti -fno-exceptions"
+#export CXXFLAGS="--sysroot=$SYSROOT -Wall -g -fno-rtti -fno-exceptions"
 
 export LDFLAGS="-L$SYSROOT/usr/lib -L$BOINCROOTDIR/lib"
 
-export CPPFLAGS="-I$DESTDIR/include -DANDROID=1 -I$NDKPATH/sources/cxx-stl/gnu-libstdc++/include/ -I$NDKPATH/sources/cxx-stl/gnu-libstdc++/libs/armeabi/include/"
+export CPPFLAGS="-I$DESTDIR/include -DANDROID=1 -I$NDKPATH/sources/cxx-stl/gnu-libstdc++/include/ -I$NDKPATH/sources/cxx-stl/gnu-libstdc++/libs/armeabi/include/ -I$BOINCROOTDIR/include"
 export STDCPP="$NDKPATH/sources/cxx-stl/gnu-libstdc++/libs/armeabi/libstdc++.a"
 #export CPPFLAGS="-I$BOINCROOTDIR/include -DANDROID=1 -I$NDKPATH/sources/cxx-stl/gnu-libstdc++/include/ -I$NDKPATH/sources/cxx-stl/gnu-libstdc++/libs/armeabi-v7a/include/"
 #export STDCPP="$NDKPATH/sources/cxx-stl/gnu-libstdc++/libs/armeabi-v7a/libstdc++.a"
